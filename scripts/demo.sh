e88a038ba13e788598ba5458742e835c15d09504
Mathias Reker <26626066+MathiasReker@users.noreply.github.com>
2025-07-02 17:58:33 +0200

    improve the script

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,21 +1,37 @@
 #!/usr/bin/env bash
 
-echo "> # Welcome to PHP SVG Optimizer!" | pv -qL 10
+set -e
+
+type_effect() {
+  echo "$1" | pv -qL 10
+}
+
+type_effect "> # Welcome to PHP SVG Optimizer!"
 sleep 1
-echo "> # See how easy it is to optimize SVG files" | pv -qL 10
+
+type_effect "> # See how easy it is to optimize SVG files"
 sleep 1
-echo "> # Let's start by installing the library" | pv -qL 10
+
+type_effect "> # Let's start by installing the library"
 sleep 1
-cd ..
-echo "> composer require mathiasreker/php-svg-optimizer" | pv -qL 10
+
+cd .. || { echo "Failed to change directory"; exit 1; }
+
+type_effect "> composer require mathiasreker/php-svg-optimizer"
 composer require mathiasreker/php-svg-optimizer
+
 echo ">"
 echo ">"
-echo "> # You're ready to optimize your SVG files!" | pv -qL 10
-echo "> # Run the following command to process the SVG files in a directory:" | pv -qL 10
-echo "> vendor/bin/svg-optimizer process php-svg-optimizer/assets/logos" | pv -qL 10
+
+type_effect "> # You're ready to optimize your SVG files!"
+type_effect "> # Run the following command to process the SVG files in a directory:"
+type_effect "> vendor/bin/svg-optimizer process php-svg-optimizer/assets/logos"
+
 vendor/bin/svg-optimizer process php-svg-optimizer/assets/logos
+
 echo ">"
 echo ">"
-echo "> # That's it! Your SVG files are now optimized" | pv -qL 10
-sleep 20
+
+type_effect "> # That's it! Your SVG files are now optimized"
+
+sleep 2
