12bc7c7eacf31ea6ae4e3d46744e4aebdd4d8c2d
Mathias Reker <26626066+MathiasReker@users.noreply.github.com>
2025-10-24 01:46:17 +0200

    fix typo

--- src/Type/Application.php
+++ src/Type/Application.php
@@ -17,6 +17,6 @@ namespace MathiasReker\PhpSvgOptimizer\Type;
 enum Application: string
 {
     case NAME = 'PHP SVG Optimizer';
-    case VERSION = '7.2.0r';
+    case VERSION = '7.2.0';
     case AUTHOR = 'Mathias Reker';
 }
