b0c4d307a0380efedff57a8bbf8f9f8a4904a81b
Mathias Reker <26626066+MathiasReker@users.noreply.github.com>
2025-11-17 20:25:53 +0100

    build badges

--- dev/scripts/badges.sh
+++ dev/scripts/badges.sh
@@ -4,22 +4,22 @@ set -euo pipefail
 
 mkdir -p dev/artifacts
 
-# Badge: Filesize
 FILES=(bin src composer.json composer.lock LICENSE)
 SIZE_KB=$(du -sk "${FILES[@]}" 2>/dev/null | awk '{sum+=$1} END {print sum}')
-curl -s -o dev/artifacts/filesize.svg "https://badgen.net/badge/size/${SIZE_KB}KB/green?label=size"
 
-# Badge: Code Coverage
+curl -s -o dev/artifacts/filesize.svg \
+  "https://img.shields.io/badge/size-${SIZE_KB}KB-brightgreen?style=flat"
 FRACTION=$(./vendor/bin/phpunit --coverage-text | grep 'Lines:' | head -1 | awk '{print $3}')
 
 [[ $FRACTION == .* ]] && FRACTION="0$FRACTION"
+
 PERCENT_INT=$(printf "%.0f" "$(echo "$FRACTION * 100" | awk '{printf "%0.0f", $1}')")
 
 COLOR="red"
 (( PERCENT_INT >= 80 )) && COLOR="green"
 (( PERCENT_INT >= 50 && PERCENT_INT < 80 )) && COLOR="yellow"
 
-curl -s -o dev/artifacts/coverage.svg "https://badgen.net/badge/code%20coverage/${PERCENT_INT}%25/${COLOR}"
+curl -s -o dev/artifacts/coverage.svg \
+  "https://img.shields.io/badge/code_coverage-${PERCENT_INT}%25-${COLOR}?style=flat"
 
-# Optimize svg files
 php bin/svg-optimizer -q process dev/artifacts
