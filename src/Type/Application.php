525675ea441de5ad9afe2c985d57fe9d7338b73b
Mathias Reker <26626066+MathiasReker@users.noreply.github.com>
2025-11-17 21:20:35 +0100

    bump version

--- src/Type/Application.php
+++ src/Type/Application.php
@@ -17,6 +17,6 @@ namespace MathiasReker\PhpSvgOptimizer\Type;
 enum Application: string
 {
     case NAME = 'PHP SVG Optimizer';
-    case VERSION = '8.0';
+    case VERSION = '8.0.1';
     case AUTHOR = 'Mathias Reker';
 }
