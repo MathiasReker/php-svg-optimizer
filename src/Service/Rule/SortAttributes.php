e1f5540bbbbc63a738df036890ce5e292938c184
Mathias Reker <26626066+MathiasReker@users.noreply.github.com>
2026-01-21 17:21:21 +0100

    update dependencies

--- src/Service/Rule/SortAttributes.php
+++ src/Service/Rule/SortAttributes.php
@@ -79,9 +79,9 @@ final readonly class SortAttributes implements SvgOptimizerRuleInterface
         $attributes = $this->extractAttributes($domElement);
         $sortedAttributes = $this->sortAttributes($attributes);
 
-        /** @var \DOMAttr $attribute */
-        foreach (iterator_to_array($domElement->attributes, false) as $attribute) {
-            $domElement->removeAttribute($attribute->name);
+        /** @var \DOMAttr $domAttr */
+        foreach (iterator_to_array($domElement->attributes, false) as $domAttr) {
+            $domElement->removeAttribute($domAttr->name);
         }
 
         foreach ($sortedAttributes as $name => $value) {
